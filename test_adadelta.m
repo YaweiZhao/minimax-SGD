ca2bdd020568376eba63a2627828b1b17c262742
Yawei Zhao <zhaoyawei@nudt.edu.cn>
2018-02-26 15:55:52 -0500

    d
    d

--- test_adadelta.m
+++ test_adadelta.m
@@ -12,8 +12,6 @@ training_label = label(n_test+1:n,:);
 %label(label==2) = -1;% all the labels are +1 or -1.
 test_data = data(1:n_test,2:d);
 training_data = data(n_test+1:n,2:d);
-%training_data = transpose(mapstd(training_data'));
-%training_data = [training_data ones(n,1)];% add 1-offset
 [n_train,d] = size(training_data);
 
 %% initialize variables
